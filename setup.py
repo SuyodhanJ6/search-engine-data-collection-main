0955f6bcee22e7489bfb1c26f060ecf188024e3d
SuyodhanJ6 <prashantmalge181@gmail.com>
2023-04-26 21:52:24 +0530

    Second Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prashant Malge",
+    author_email="prashantmalge181.com",
+    packages=find_packages()
+)
